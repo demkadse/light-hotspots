5fc4d699712032bddf4060aba917b7f3fc2bfa81
demkadse <163359015+demkadse@users.noreply.github.com>
2026-03-18 09:12:31 +0100

    Update registerCommands.js

--- bot/registerCommands.js
+++ bot/registerCommands.js
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// 👉 HIER EINTRAGEN
 const CLIENT_ID = "1483031945825747034";
 const GUILD_ID = "1483034141858598965";
 
@@ -11,7 +10,6 @@ const commands = [
   new SlashCommandBuilder()
     .setName("setup-events")
     .setDescription("Erstellt das Event Panel")
-    .toJSON()
 ];
 
 const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
@@ -20,20 +18,14 @@ async function register() {
 
   try {
 
-    console.log("Token vorhanden:", !!process.env.DISCORD_TOKEN);
-    console.log("Client ID:", CLIENT_ID);
-    console.log("Guild ID:", GUILD_ID);
+    console.log("Registriere Guild Commands...");
 
-    console.log("Registriere Commands...");
-
-    const response = await rest.put(
+    const result = await rest.put(
       Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
-      { body: commands }
+      { body: commands.map(cmd => cmd.toJSON()) }
     );
 
-    console.log("Discord Response:", response);
-
-    console.log("FERTIG.");
+    console.log("RESULT:", result);
 
   } catch (error) {
 
