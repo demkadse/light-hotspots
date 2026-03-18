95875412776a544f72ced16adbeebedee5c61c33
demkadse <163359015+demkadse@users.noreply.github.com>
2026-03-18 08:58:05 +0100

    Create setupEventPanel.js

---
+++ bot/commands/setupEventPanel.js
@@ -0,0 +1,20 @@
+import { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } from "discord.js";
+
+export const data = new SlashCommandBuilder()
+  .setName("setup-events")
+  .setDescription("Erstellt das Event Panel");
+
+export async function execute(interaction) {
+
+  const button = new ButtonBuilder()
+    .setCustomId("create_event")
+    .setLabel("Event erstellen")
+    .setStyle(ButtonStyle.Primary);
+
+  const row = new ActionRowBuilder().addComponents(button);
+
+  await interaction.reply({
+    content: "📅 **Event-System**\nErstelle hier neue Events.",
+    components: [row]
+  });
+}
