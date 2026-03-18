55e0d740fddc94373e6fc6dd0e0a6cc86e35c40d
demkadse <163359015+demkadse@users.noreply.github.com>
2026-03-18 12:18:57 +0100

    fix

--- js/modal.js
+++ js/modal.js
@@ -5,17 +5,17 @@ function openModal(event){
 
 modalContent.innerHTML = `
 
-<img src="${event.image}" style="width:100%">
+<img src="${event.image || ""}" style="width:100%">
 
-<h2>${event.title}</h2>
+<h2>${event.title || "Unbenannt"}</h2>
 
-<p><b>Venue:</b> ${event.venue}</p>
+<p><b>Venue:</b> ${event.venue || "-"}</p>
 
-<p><b>Host:</b> ${event.host}</p>
+<p><b>Host:</b> ${event.created_by || "-"}</p>
 
-<p>${event.date} ${event.start_time}${event.end_time ? " - "+event.end_time : ""}</p>
+<p>${event.date || ""} ${event.time || ""}</p>
 
-<p>${event.description}</p>
+<p>${event.description || ""}</p>
 
 `;
 
@@ -24,9 +24,7 @@ modal.classList.add("active");
 }
 
 function closeModal(){
-
 modal.classList.remove("active");
-
 }
 
 document.getElementById("modal-close").onclick = closeModal;
